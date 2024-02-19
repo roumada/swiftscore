6c3edc1a583a959464e55266a2ae7c4f1ada8e6b
roumada <catshapedyuegui@mailbox.org>
2024-02-19 17:33:17 +0100

    Intial commit

---
+++ src/main/java/com/roumada/swiftscore/SwiftscoreApplication.java
@@ -0,0 +1,13 @@
+package com.roumada.swiftscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roumada/swiftscore/SwiftscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roumada.swiftscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
