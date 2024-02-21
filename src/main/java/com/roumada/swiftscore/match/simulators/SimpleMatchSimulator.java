7df65feef64c9f2ac42deaeedb60b05a3ae8a0cc
roumada <catshapedyuegui@mailbox.org>
2024-02-21 13:53:08 +0100

    Testing - yellow tests

--- src/main/java/com/roumada/swiftscore/match/simulators/SimpleMatchSimulator.java
+++ src/main/java/com/roumada/swiftscore/match/simulators/SimpleMatchSimulator.java
@@ -7,6 +7,15 @@ public class SimpleMatchSimulator implements MatchSimulator {
 
     @Override
     public void simulateMatch(FootballMatch footballMatch) {
+        float homeSideVictoryChance = footballMatch.getHomeSideStatistics().getFootballClub().getVictoryChance();
+        float awaySideVictoryChance = footballMatch.getAwaySideStatistics().getFootballClub().getVictoryChance();
 
+        if (homeSideVictoryChance == awaySideVictoryChance) {
+            footballMatch.setMatchStatus(FootballMatch.Status.DRAW);
+        } else if (homeSideVictoryChance > awaySideVictoryChance) {
+            footballMatch.setMatchStatus(FootballMatch.Status.HOME_SIDE_VICTORY);
+        } else {
+            footballMatch.setMatchStatus(FootballMatch.Status.AWAY_SIDE_VICTORY);
+        }
     }
 }
