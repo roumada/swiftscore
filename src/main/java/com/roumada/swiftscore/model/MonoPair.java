37efb00e49aa91e8ec716e6174a6d1df19a6fbc6
roumada <taixisoftworks@mailbox.org>
2024-02-26 11:25:14 +0100

    Add match week populator

---
+++ src/main/java/com/roumada/swiftscore/model/MonoPair.java
@@ -0,0 +1,35 @@
+package com.roumada.swiftscore.model;
+
+import lombok.Getter;
+
+/**
+ * An implementation of a single-type pair.
+ * @param <T> preferred pair type
+ */
+@Getter
+public class MonoPair<T> {
+
+    private final T a;
+    private final T b;
+
+    private MonoPair(T a, T b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static <T> MonoPair<T> of (T a, T b) {
+        return new MonoPair<>(a, b);
+    }
+
+    public MonoPair<T> invert() {
+        return new MonoPair<>(b, a);
+    }
+
+    @Override
+    public String toString() {
+        return "MonoPair{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
