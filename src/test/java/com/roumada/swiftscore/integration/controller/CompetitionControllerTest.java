19ad1039a1d1dc0035b0ae5bb87e991ec05ba1ac
roumada <taixisoftworks@mailbox.org>
2024-12-10 17:08:05 +0100

    Write remaining tests

--- src/test/java/com/roumada/swiftscore/integration/controller/CompetitionControllerTest.java
+++ src/test/java/com/roumada/swiftscore/integration/controller/CompetitionControllerTest.java
@@ -3,7 +3,10 @@ package com.roumada.swiftscore.integration.controller;
 import com.roumada.swiftscore.integration.AbstractBaseIntegrationTest;
 import com.roumada.swiftscore.model.FootballClub;
 import com.roumada.swiftscore.model.dto.CompetitionDTO;
+import com.roumada.swiftscore.model.match.Competition;
+import com.roumada.swiftscore.persistence.repository.CompetitionRepository;
 import com.roumada.swiftscore.persistence.repository.FootballClubRepository;
+import org.json.JSONArray;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +31,8 @@ class CompetitionControllerTest extends AbstractBaseIntegrationTest {
     private MockMvc mvc;
     @Autowired
     private FootballClubRepository fcrepository;
+    @Autowired
+    private CompetitionRepository competitionRepository;
 
     @Test
     @DisplayName("Should create a competition if there are clubs with given IDs in the database")
@@ -51,5 +57,22 @@ class CompetitionControllerTest extends AbstractBaseIntegrationTest {
 
         mvc.perform(get("/competition/" + compId))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should return all competitions")
+    void shouldReturnAllCompetitions() throws Exception {
+        competitionRepository.save(new Competition());
+        competitionRepository.save(new Competition());
+
+        var result = mvc.perform(get("/competition/all"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var resultArray = new JSONArray(result);
+
+        assertEquals(2, resultArray.length());
     }
 }
