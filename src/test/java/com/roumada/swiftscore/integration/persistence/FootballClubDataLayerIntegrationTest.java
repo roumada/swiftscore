b2f8618533d4845f914aa63111ba76db9906d11d
roumada <taixisoftworks@mailbox.org>
2024-12-10 16:49:37 +0100

    Convert repository tests to data layer tests 2

--- src/test/java/com/roumada/swiftscore/integration/persistence/FootballClubDataLayerIntegrationTest.java
+++ src/test/java/com/roumada/swiftscore/integration/persistence/FootballClubDataLayerIntegrationTest.java
@@ -2,7 +2,7 @@ package com.roumada.swiftscore.integration.persistence;
 
 import com.roumada.swiftscore.integration.AbstractBaseIntegrationTest;
 import com.roumada.swiftscore.model.FootballClub;
-import com.roumada.swiftscore.persistence.repository.FootballClubRepository;
+import com.roumada.swiftscore.persistence.FootballClubDataLayer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +14,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FootballClubDataLayerIntegrationTest extends AbstractBaseIntegrationTest {
 
     @Autowired
-    private FootballClubRepository repository;
+    private FootballClubDataLayer dataLayer;
 
     @Test
     @DisplayName("Should save a football club to the database")
-    void shoudSaveToDatabase() {
-        FootballClub fc = FootballClub.builder().id(null).name("Norf FC").victoryChance(0.33f).build();
+    void shouldSaveToDatabase() {
+        // arrange
+        FootballClub fc = FootballClub.builder().name("Norf FC").victoryChance(0.33f).build();
 
-        FootballClub saved = repository.save(fc);
-        Optional<FootballClub> optionalFC = repository.findById(saved.getId());
+        FootballClub saved = dataLayer.save(fc);
+        Optional<FootballClub> optionalFC = dataLayer.findById(saved.getId());
 
         assertThat(optionalFC).isPresent();
 
---
+++ src/test/java/com/roumada/swiftscore/integration/persistence/FootballMatchDataLayerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.roumada.swiftscore.integration.persistence;
+
+import com.roumada.swiftscore.integration.AbstractBaseIntegrationTest;
+import com.roumada.swiftscore.model.match.FootballMatch;
+import com.roumada.swiftscore.model.match.FootballMatchStatistics;
+import com.roumada.swiftscore.persistence.FootballMatchDataLayer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FootballMatchDataLayerIntegrationTest extends AbstractBaseIntegrationTest {
+
+    @Autowired
+    private FootballMatchDataLayer dataLayer;
+
+    @Test
+    @DisplayName("Should save a football match to the database")
+    void shouldSaveAMatchToDatabase() {
+        // arrange
+        var stats1 = new FootballMatchStatistics(null);
+        var stats2 = new FootballMatchStatistics(null);
+        var match = new FootballMatch(stats1, stats2);
+
+        // act
+        var saved = dataLayer.saveMatch(match);
+        Optional<FootballMatch> optionalMatch = dataLayer.findMatchById(saved.getId());
+
+        // assert
+        assertThat(optionalMatch).isPresent();
+        var retrievedMatch = optionalMatch.get();
+        assertThat(retrievedMatch.getId()).isEqualTo(saved.getId());
+    }
+}
