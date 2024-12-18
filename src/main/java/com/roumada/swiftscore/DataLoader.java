4a204a537c03d5d6615d008b990f56fb0a725d67
roumada <160512774+roumada@users.noreply.github.com>
2024-12-18 18:53:58 +0100

    Taiga-12 Add sample data to database upon application start (#13)
    * Add sample data to database upon application start
    
    * Add Github Actions file

---
+++ src/main/java/com/roumada/swiftscore/DataLoader.java
@@ -0,0 +1,48 @@
+package com.roumada.swiftscore;
+
+import com.roumada.swiftscore.model.FootballClub;
+import com.roumada.swiftscore.persistence.FootballClubDataLayer;
+import com.roumada.swiftscore.persistence.repository.FootballClubRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final MongoTemplate mongoTemplate;
+    private final FootballClubRepository footballClubRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        dropPrevious();
+        saveFCs();
+    }
+
+    private void dropPrevious() {
+        mongoTemplate.getDb().drop();
+        log.info("Previous database dropped");
+    }
+
+    private void saveFCs() {
+        footballClubRepository.saveAll(List.of(
+                FootballClub.builder().name("FC1").victoryChance(0.1).build(),
+                FootballClub.builder().name("FC2").victoryChance(0.2).build(),
+                FootballClub.builder().name("FC3").victoryChance(0.3).build(),
+                FootballClub.builder().name("FC4").victoryChance(0.4).build(),
+                FootballClub.builder().name("FC5").victoryChance(0.5).build(),
+                FootballClub.builder().name("FC6").victoryChance(0.6).build(),
+                FootballClub.builder().name("FC7").victoryChance(0.7).build(),
+                FootballClub.builder().name("FC8").victoryChance(0.8).build(),
+                FootballClub.builder().name("FC9").victoryChance(0.9).build(),
+                FootballClub.builder().name("FC10").victoryChance(1).build()
+        ));
+        log.info("Football clubs saved");
+    }
+}
