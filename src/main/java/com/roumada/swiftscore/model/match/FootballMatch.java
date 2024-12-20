f54125f28734cb34eb2d98662e64851701f8ed98
roumada <taixisoftworks@mailbox.org>
2024-12-20 14:32:36 +0100

    Removed unused annotation in FootballMatch

--- src/main/java/com/roumada/swiftscore/model/match/FootballMatch.java
+++ src/main/java/com/roumada/swiftscore/model/match/FootballMatch.java
@@ -1,14 +1,12 @@
 package com.roumada.swiftscore.model.match;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document("football_match")
-@NoArgsConstructor
 public class FootballMatch {
 
     @Id
