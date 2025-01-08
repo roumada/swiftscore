cdd9fc64967f5e8d73586f89f5c380f827b7dd7c
roumada <taixisoftworks@mailbox.org>
2025-01-08 14:13:47 +0100

    Add constructor

--- src/main/java/com/roumada/swiftscore/model/match/CompetitionRound.java
+++ src/main/java/com/roumada/swiftscore/model/match/CompetitionRound.java
@@ -2,6 +2,7 @@ package com.roumada.swiftscore.model.match;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @Document("competition_round")
 public class CompetitionRound {
 
