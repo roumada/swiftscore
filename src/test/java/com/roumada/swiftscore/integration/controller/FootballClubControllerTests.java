90a715d0606364520f176b01dfd2d6c3b8ee3183
roumada <taixisoftworks@mailbox.org>
2025-01-14 13:39:11 +0100

    Add tests

--- src/test/java/com/roumada/swiftscore/integration/controller/FootballClubControllerTests.java
+++ src/test/java/com/roumada/swiftscore/integration/controller/FootballClubControllerTests.java
@@ -1,7 +1,10 @@
 package com.roumada.swiftscore.integration.controller;
 
 import com.roumada.swiftscore.integration.AbstractBaseIntegrationTest;
+import com.roumada.swiftscore.model.FootballClub;
 import com.roumada.swiftscore.model.dto.FootballClubDTO;
+import com.roumada.swiftscore.persistence.FootballClubDataLayer;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,13 +26,15 @@ class FootballClubControllerTests extends AbstractBaseIntegrationTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private FootballClubDataLayer footballClubDataLayer;
 
     @Test
     @DisplayName("Create football club - with valid data - should save")
     void createFootballClub_validData_shouldSave() throws Exception {
         // arrange
         var dto = new FootballClubDTO("FC1", 0.5f);
-        // act
+        // act & assert
         mvc.perform(post("/footballclub").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isOk());
@@ -40,7 +45,7 @@ class FootballClubControllerTests extends AbstractBaseIntegrationTest {
     void createFootballClub_invalidVictoryChance_shouldReturnErrorCode() throws Exception {
         // arrange
         var dto = new FootballClubDTO("FC1", 2.2f);
-        // act
+        // act & assert
         mvc.perform(post("/footballclub").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().is4xxClientError());
@@ -51,7 +56,7 @@ class FootballClubControllerTests extends AbstractBaseIntegrationTest {
     void createFootballClub_missingName_shouldReturnErrorCode() throws Exception {
         // arrange
         var dto = new FootballClubDTO(null, 2.2f);
-        // act
+        // act & assert
         mvc.perform(post("/footballclub").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().is4xxClientError());
@@ -61,17 +66,10 @@ class FootballClubControllerTests extends AbstractBaseIntegrationTest {
     @DisplayName("Get football club - with valid ID - should return")
     void getFootballClub_validId_shouldReturn() throws Exception {
         // arrange
-        var dto = new FootballClubDTO("FC1", 0.5f);
+        var fcID = footballClubDataLayer.save(new FootballClub("FC1", 0.5f)).getId();
+
         // act
-        var response = mvc.perform(post("/footballclub").contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse().getContentAsString();
-
-        var fcID = new JSONObject(response).getLong("id");
-
-        response = mvc.perform(get("/footballclub/" + fcID))
+        var response = mvc.perform(get("/footballclub/" + fcID))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse().getContentAsString();
@@ -84,13 +82,28 @@ class FootballClubControllerTests extends AbstractBaseIntegrationTest {
     @DisplayName("Get football club - with invalid ID - should return error code")
     void getFootballClub_invalidId_shouldReturnErrorCode() throws Exception {
         // arrange
-        var dto = new FootballClubDTO("FC1", 0.5f);
-        // act
-        mvc.perform(post("/footballclub").contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isOk());
+        footballClubDataLayer.save(new FootballClub("FC1", 0.5f));
 
+        // act & assert
         mvc.perform(get("/footballclub/" + 999))
                 .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("Get all football clubs  - should return")
+    void getAllFootballClubs_shouldReturn() throws Exception {
+        // arrange
+        footballClubDataLayer.save(new FootballClub("FC1", 0.5f));
+        footballClubDataLayer.save(new FootballClub("FC2", 0.4f));
+
+        // act
+        var response = mvc.perform(get("/footballclub/all"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+        var responseJSON = new JSONArray(response);
+
+        // assert
+        assertEquals(2, responseJSON.length());
     }
 }
