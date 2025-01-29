3da48af366085912cfe2776e232e25546a580688
roumada <taixisoftworks@mailbox.org>
2025-01-29 15:35:31 +0100

    Add CompetitionRoundMapperTests

--- src/main/java/com/roumada/swiftscore/model/mapper/CompetitionRoundMapper.java
+++ src/main/java/com/roumada/swiftscore/model/mapper/CompetitionRoundMapper.java
@@ -3,13 +3,11 @@ package com.roumada.swiftscore.model.mapper;
 import com.roumada.swiftscore.model.dto.response.CompetitionRoundResponseDTO;
 import com.roumada.swiftscore.model.match.CompetitionRound;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = FootballMatchMapper.class)
 public interface CompetitionRoundMapper {
     CompetitionRoundMapper INSTANCE = Mappers.getMapper(CompetitionRoundMapper.class);
 
-    @Mapping(source = "matches", target = "matches")
     CompetitionRoundResponseDTO roundToResponseDTO(CompetitionRound round);
 }
