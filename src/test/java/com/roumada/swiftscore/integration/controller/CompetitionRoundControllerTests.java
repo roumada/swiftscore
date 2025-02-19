876742218d38be581b425a78f6a68aff06947e62
roumada <taixisoftworks@mailbox.org>
2025-02-19 13:02:44 +0100

    Revise CompetitionRoundControllerTests

--- src/test/java/com/roumada/swiftscore/integration/controller/CompetitionRoundControllerTests.java
+++ src/test/java/com/roumada/swiftscore/integration/controller/CompetitionRoundControllerTests.java
@@ -1,8 +1,10 @@
 package com.roumada.swiftscore.integration.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.roumada.swiftscore.integration.AbstractBaseIntegrationTest;
 import com.roumada.swiftscore.model.match.CompetitionRound;
-import com.roumada.swiftscore.persistence.CompetitionRoundDataLayer;
+import com.roumada.swiftscore.persistence.repository.CompetitionRoundRepository;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,40 +14,51 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 class CompetitionRoundControllerTests extends AbstractBaseIntegrationTest {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     private MockMvc mvc;
     @Autowired
-    private CompetitionRoundDataLayer competitionRoundDataLayer;
+    private CompetitionRoundRepository repository;
 
     @Test
     @DisplayName("Get competition round - valid ID - should return")
     void getCompetitionRound_validId_shouldReturn() throws Exception {
         // arrange
-        CompetitionRound round = new CompetitionRound(1L, 1, Collections.emptyList());
-        var roundId = competitionRoundDataLayer.save(round).getId();
+        var roundId = repository.save(new CompetitionRound(1L, 1, Collections.emptyList())).getId();
 
         // act
-        var result = mvc.perform(get("/competition/round/" + roundId))
+        var response = mvc.perform(get("/competition/round/" + roundId))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse().getContentAsString();
 
         // assert
-        var resultJSON = new JSONObject(result);
-        assertEquals(roundId, resultJSON.getLong("id"));
+        var round = objectMapper.readValue(response, CompetitionRound.class);
+        assertThat(round.getId()).isEqualTo(roundId);
     }
 
     @Test
     @DisplayName("Get competition round - invalid ID - should return error code")
     void getCompetitionRound_invalidId_shouldReturnError() throws Exception {
-        mvc.perform(get("/competition/round/" + 111))
-                .andExpect(status().is4xxClientError());
+        // arrange
+        var id = 111;
+
+        // act
+        var response = mvc.perform(get("/competition/round/" + id))
+                .andExpect(status().is4xxClientError())
+                .andReturn().getResponse().getContentAsString();
+
+        // assert
+        JSONArray validationErrors = new JSONObject(response).getJSONArray("validationErrors");
+        assertThat(validationErrors.length()).isEqualTo(1);
+        assertThat(validationErrors.get(0))
+                .isEqualTo("Competition round with ID [%s] not found.".formatted(id));
     }
 }
