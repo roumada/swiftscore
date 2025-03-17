e26f9b256cc3c1463c3cc09436397c1c52ce75fb
roumada <160512774+roumada@users.noreply.github.com>
2025-03-17 11:35:59 +0100

    TAIGA-53 Remove MonoPair from project (#42)

--- src/main/java/com/roumada/swiftscore/logic/competition/CompetitionRoundsCreator.java
+++ src/main/java/com/roumada/swiftscore/logic/competition/CompetitionRoundsCreator.java
@@ -1,11 +1,11 @@
 package com.roumada.swiftscore.logic.competition;
 
 import com.roumada.swiftscore.model.FootballClub;
-import com.roumada.swiftscore.model.MonoPair;
-import com.roumada.swiftscore.model.organization.CompetitionRound;
 import com.roumada.swiftscore.model.match.FootballMatch;
+import com.roumada.swiftscore.model.organization.CompetitionRound;
 import io.vavr.control.Either;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,20 +24,20 @@ public class CompetitionRoundsCreator {
             return Either.left(errorMsg);
         }
 
-        List<List<MonoPair<Integer>>> numericRoundRobinMatchups = generateRoundRobinNumericMatchups(clubs.size());
+        List<List<Pair<Integer, Integer>>> numericRoundRobinMatchups = generateRoundRobinNumericMatchups(clubs.size());
         return Either.right(createCompetitionRoundsOnNumericMatchups(clubs, numericRoundRobinMatchups));
     }
 
     private static List<CompetitionRound> createCompetitionRoundsOnNumericMatchups(List<FootballClub> clubs,
-                                                                                   List<List<MonoPair<Integer>>> numericRoundRobinMatchups) {
+                                                                                   List<List<Pair<Integer, Integer>>> numericRoundRobinMatchups) {
         List<CompetitionRound> competitionRounds = new ArrayList<>();
         int roundCounter = 1;
 
-        for (List<MonoPair<Integer>> numericCompRound : numericRoundRobinMatchups) {
+        for (List<Pair<Integer, Integer>> numericCompRound : numericRoundRobinMatchups) {
             List<FootballMatch> compRoundMatches = new ArrayList<>();
 
-            for (MonoPair<Integer> numericMatch : numericCompRound) {
-                compRoundMatches.add(new FootballMatch(clubs.get(numericMatch.getLeft()), clubs.get(numericMatch.getRight())));
+            for (Pair<Integer, Integer> numericMatch : numericCompRound) {
+                compRoundMatches.add(new FootballMatch(clubs.get(numericMatch.getFirst()), clubs.get(numericMatch.getSecond())));
             }
             competitionRounds.add(new CompetitionRound(roundCounter++, compRoundMatches));
         }
@@ -45,14 +45,14 @@ public class CompetitionRoundsCreator {
         return competitionRounds;
     }
 
-    private static List<List<MonoPair<Integer>>> generateRoundRobinNumericMatchups(int leagueSize) {
+    private static List<List<Pair<Integer, Integer>>> generateRoundRobinNumericMatchups(int leagueSize) {
         int totalRounds = leagueSize - 1;
 
-        List<List<MonoPair<Integer>>> allCompRoundsIds = new ArrayList<>();
+        List<List<Pair<Integer, Integer>>> allCompRoundsIds = new ArrayList<>();
 
         for (int round = 0; round < totalRounds; round++) {
-            List<MonoPair<Integer>> compRoundIds = new ArrayList<>();
-            List<MonoPair<Integer>> invertedCompRoundIds = new ArrayList<>();
+            List<Pair<Integer, Integer>> compRoundIds = new ArrayList<>();
+            List<Pair<Integer, Integer>> invertedCompRoundIds = new ArrayList<>();
             for (int match = 0; match < leagueSize / 2; match++) {
                 int home = (round + match) % (leagueSize - 1);
                 int away = (leagueSize - 1 - match + round) % (leagueSize - 1);
@@ -61,8 +61,8 @@ public class CompetitionRoundsCreator {
                     away = leagueSize - 1;
                 }
 
-                compRoundIds.add(MonoPair.of(home, away));
-                invertedCompRoundIds.add(MonoPair.of(away, home));
+                compRoundIds.add(Pair.of(home, away));
+                invertedCompRoundIds.add(Pair.of(away, home));
             }
             allCompRoundsIds.add(compRoundIds);
             allCompRoundsIds.add(invertedCompRoundIds);
